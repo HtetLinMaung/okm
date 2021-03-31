439b2ccf1409b8332bb53d00742c8d268d63f653
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2021-03-31 10:23:57 +0630

    update

---
+++ src/main/java/com/nirvasoft/web/okm/controllers/AngularController.java
@@ -0,0 +1,13 @@
+package com.nirvasoft.web.okm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AngularController {
+    @GetMapping(path = { "angular/**", "angular/" })
+    public String index() {
+        return "index.html";
+    }
+
+}
--- src/main/java/com/nirvasoft/web/okm/dao/CustomerRegistrationDao.java
+++ src/main/java/com/nirvasoft/web/okm/dao/CustomerRegistrationDao.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
-import java.util.Optional;
 
 import com.nirvasoft.web.okm.models.AddressData;
 import com.nirvasoft.web.okm.models.AdvanceSearchData;
@@ -43,79 +42,79 @@ public class CustomerRegistrationDao extends QueryUtil {
     }
 
     public int registerCustomer(CustomerData data) {
-        try {
-            String keyNames = "CustomerId, CustomerType, Title, Name, AliasName, Sex, DateOfBirth, NrcNo, HouseNo, Street, Ward, BuildingName, Township, Division, Country, Phone, Email, Fax, PostalCode, Status, Occupation, FatherName, Universalid, MStatus, OldNrcNo, M1, M2, T2, BCNo, PassportNo, NationalityStatus, IsHasDoc, IsSameAddress, PHouseNo, PStreet, PWard, PTownship, PDivision, PCountry, PPostalCode, RegistrationDate, PPhone, PEmail, PFax, PBuildingName, RegisterType, T6";
-            final String[] keys = keyNames.split(",");
 
-            String sql = "INSERT INTO Customer (" + keyNames + ") VALUES (";
-            for (int i = 0; i < keys.length; i++) {
-                sql += "?";
-                if (i != keys.length - 1) {
-                    sql += ", ";
-                }
+        String keyNames = "CustomerId, CustomerType, Title, Name, AliasName, Sex, DateOfBirth, NrcNo, HouseNo, Street, Ward, BuildingName, Township, Division, Country, Phone, Email, Fax, PostalCode, Status, Occupation, FatherName, Universalid, MStatus, OldNrcNo, M1, M2, T2, BCNo, PassportNo, NationalityStatus, IsHasDoc, IsSameAddress, PHouseNo, PStreet, PWard, PTownship, PDivision, PCountry, PPostalCode, RegistrationDate, PPhone, PEmail, PFax, PBuildingName, RegisterType, T6";
+        final String[] keys = keyNames.split(",");
+
+        String sql = "INSERT INTO Customer (" + keyNames + ") VALUES (";
+        for (int i = 0; i < keys.length; i++) {
+            sql += "?";
+            if (i != keys.length - 1) {
+                sql += ", ";
             }
-            sql += ")";
-
-            long id = getNextId("Customer", "CustomerId", jdbcTemplate);
-
-            int success = jdbcTemplate.update(sql,
-                    new Object[] { id, data.getCustomerType(), data.getTitle(), data.getName(), data.getAliasName(),
-                            data.getSex(), data.getDateOfBirth(), data.getNrcNo(), data.getHouseNo(), data.getStreet(),
-                            data.getWard(), data.getBuildingName(), data.getTownship(), data.getDivision(),
-                            data.getCountry(), data.getPhone(), data.getEmail(), ";;", data.getPostalCode(), "Save",
-                            data.getOccupation(), data.getFatherName(), 999, data.getmStatus(), "", "::", "Buddhist",
-                            data.getT2(), "BC", data.getPassportNo(), data.getNationalityStatus(), data.getIsHasDoc(),
-                            1, data.getHouseNo(), data.getStreet(), data.getWard(), data.getTownship(),
-                            data.getDivision(), data.getCountry(), data.getPostalCode(), data.getRegistrationDate(),
-                            data.getPhone(), data.getEmail(), ";;", data.getBuildingName(), 1, 19000101 });
-
-            if (success > 0) {
-                sql = "INSERT INTO Customer_Doc (CustomerId, DocumentType, File_Path, File_Name) VALUES (?, ?, ?, ?)";
-
-                byte[] decodedFile = Base64.getDecoder().decode(data.getPhoto().split(",")[1]);
-                ;
-                String filePath = "src" + File.separator + "main" + File.separator + "resources" + File.separator
-                        + "static" + File.separator + "cache" + File.separator + "photos" + File.separator + "customer"
-                        + File.separator;
-                FileOutputStream outputStream = new FileOutputStream(filePath + data.getFileName());
-                outputStream.write(decodedFile);
-                outputStream.close();
-
-                ArrayList<Object> args = new ArrayList<>();
-                args.add(id);
-                args.add("Photo");
-                args.add(filePath);
-                args.add(data.getFileName());
-
-                if (data.getDocuments().size() > 0) {
-                    sql += ", ";
-                }
-                for (int i = 0; i < data.getDocuments().size(); i++) {
-                    DocumentData doc = data.getDocuments().get(i);
-                    decodedFile = Base64.getDecoder().decode(doc.getFile().split(",")[1]);
-                    filePath = "src" + File.separator + "main" + File.separator + "resources" + File.separator
-                            + "static" + File.separator + "cache" + File.separator + "document" + File.separator
-                            + "customer" + File.separator;
-                    outputStream = new FileOutputStream(filePath + doc.getFileName());
-                    outputStream.write(decodedFile);
-                    outputStream.close();
-                    sql += "(?, ?, ?, ?)";
-                    if (i != data.getDocuments().size() - 1) {
-                        sql += ", ";
-                    }
-                    args.add(id);
-                    args.add("Document");
-                    args.add(filePath);
-                    args.add(doc.getFileName());
-                }
-
-                jdbcTemplate.update(sql, args.toArray());
-            }
-            return 1;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return 0;
         }
+        sql += ")";
+
+        long id = getNextId("Customer", "CustomerId", jdbcTemplate);
+
+        int success = jdbcTemplate.update(sql,
+                new Object[] { id, data.getCustomerType(), data.getTitle(), data.getName(), data.getAliasName(),
+                        data.getSex(), data.getDateOfBirth(), data.getNrcNo(), data.getHouseNo(), data.getStreet(),
+                        data.getWard(), data.getBuildingName(), data.getTownship(), data.getDivision(),
+                        data.getCountry(), data.getPhone(), data.getEmail(), ";;", data.getPostalCode(), "Save",
+                        data.getOccupation(), data.getFatherName(), 999, data.getmStatus(), "", "::", "Buddhist",
+                        data.getT2(), "BC", data.getPassportNo(), data.getNationalityStatus(), data.getIsHasDoc(), 1,
+                        data.getHouseNo(), data.getStreet(), data.getWard(), data.getTownship(), data.getDivision(),
+                        data.getCountry(), data.getPostalCode(), data.getRegistrationDate(), data.getPhone(),
+                        data.getEmail(), ";;", data.getBuildingName(), 1, 19000101 });
+
+        if (success > 0) {
+            sql = "INSERT INTO Customer_Doc (CustomerId, DocumentType, File_Path, File_Name, doc) VALUES (?, ?, ?, ?, ?)";
+
+            byte[] decodedFile = Base64.getDecoder().decode(data.getPhoto().split(",")[1]);
+            ;
+            String filePath = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "static"
+                    + File.separator + "cache" + File.separator + "photos" + File.separator + "customer"
+                    + File.separator;
+            // FileOutputStream outputStream = new FileOutputStream(filePath +
+            // data.getFileName());
+            // outputStream.write(decodedFile);
+            // outputStream.close();
+
+            ArrayList<Object> args = new ArrayList<>();
+            args.add(id);
+            args.add("Photo");
+            args.add(filePath);
+            args.add(data.getFileName());
+            args.add(decodedFile);
+
+            if (data.getDocuments().size() > 0) {
+                sql += ", ";
+            }
+            for (int i = 0; i < data.getDocuments().size(); i++) {
+                DocumentData doc = data.getDocuments().get(i);
+                decodedFile = Base64.getDecoder().decode(doc.getFile().split(",")[1]);
+                filePath = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "static"
+                        + File.separator + "cache" + File.separator + "document" + File.separator + "customer"
+                        + File.separator;
+                // outputStream = new FileOutputStream(filePath + doc.getFileName());
+                // outputStream.write(decodedFile);
+                // outputStream.close();
+                sql += "(?, ?, ?, ?, ?)";
+                if (i != data.getDocuments().size() - 1) {
+                    sql += ", ";
+                }
+                args.add(id);
+                args.add("Document");
+                args.add(filePath);
+                args.add(doc.getFileName());
+                args.add(decodedFile);
+            }
+
+            jdbcTemplate.update(sql, args.toArray());
+        }
+        return 1;
+
     }
 
     private String getCondition(AdvanceSearchData data) {
